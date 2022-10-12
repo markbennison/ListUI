bac7d1db72c5ffc331f6e381f86aed2b9ce347ce
Mark Bennison <markbennison@users.noreply.github.com>
2022-10-12 23:05:11 +0100

    Update ItemManagementSystem.cs

--- Assets/2D Inventory/Scripts/ItemManagementSystem.cs
+++ Assets/2D Inventory/Scripts/ItemManagementSystem.cs
@@ -1,119 +1,120 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ItemManagementSystem : MonoBehaviour
 {
-    [SerializeField]
-    List<Item> fullItemList = new List<Item>();
-    [SerializeField]
-    List<Item> inventoryItemList = new List<Item>();
+	[SerializeField]
+	List<Item> fullItemList = new List<Item>();
+	[SerializeField]
+	List<Item> inventoryItemList = new List<Item>();
 
-    [SerializeField]
-    GameObject itemPrefab;
+	[SerializeField]
+	GameObject itemPrefab;
 
-    [SerializeField]
-    Transform inventoryTransform;
+	[SerializeField]
+	Transform inventoryTransform;
 
-    void Start()
-    {
-        DefineItems();
-        InitialiseFullItemList();
-    }
-
-    void Update()
-    {
-        
-    }
-
-    void DefineItems()
+	void Start()
 	{
-        fullItemList.Add(new Item("Axe", 3.0f));
-        fullItemList.Add(new Item("Bandage", 0.4f));
-        fullItemList.Add(new Item("Crossbow", 4.0f));
-        fullItemList.Add(new Item("Dagger", 0.8f));
-        fullItemList.Add(new Item("Emerald", 0.2f));
-        fullItemList.Add(new Item("Fish", 2.0f));
-        fullItemList.Add(new Item("Gems", 0.3f));
-        fullItemList.Add(new Item("Hat", 0.6f));
-        fullItemList.Add(new Item("Ingot", 5.0f));
-        fullItemList.Add(new Item("Junk", 1.2f));
-    }
-
-    void InitialiseFullItemList()
-	{
-        GameObject gameObject;
-
-        for (int i = 0; i < fullItemList.Count; i++)
-		{
-            gameObject = Instantiate(itemPrefab, transform);
-            gameObject.transform.GetChild(0).GetComponent<Text>().text = fullItemList[i].Name;
-            gameObject.transform.GetChild(1).GetComponent<Text>().text = fullItemList[i].Weight.ToString();
-            gameObject.GetComponent<Button>().AddEventListener(i, ItemClicked);
-        }
+		DefineItems();
+		InitialiseFullItemList();
 	}
 
-    void InitialiseInventoryItemList()
-    {
-        ClearInventoryItemList();
-        GameObject gameObject;
-
-        for (int i = 0; i < inventoryItemList.Count; i++)
-        {
-            gameObject = Instantiate(itemPrefab, inventoryTransform);
-            gameObject.transform.GetChild(0).GetComponent<Text>().text = inventoryItemList[i].Name;
-            gameObject.transform.GetChild(1).GetComponent<Text>().text = inventoryItemList[i].Weight.ToString();
-            gameObject.GetComponent<Button>().AddEventListener(i, InventoryItemClicked);
-        }
-    }
-
-    void ClearInventoryItemList()
+	void Update()
 	{
-        foreach(Transform child in inventoryTransform)
-        {
-            GameObject.Destroy(child.gameObject);
-        }
-    }
 
-    void ItemClicked(int index)
+	}
+
+	void DefineItems()
 	{
-        Debug.Log("Item Cicked: " + index +". " + fullItemList[index].Name + " (" + fullItemList[index].Weight + ")");
-        AddItemToInventory(index);
+		fullItemList.Add(new Item("Axe", 3.0f));
+		fullItemList.Add(new Item("Bandage", 0.4f));
+		fullItemList.Add(new Item("Crossbow", 4.0f));
+		fullItemList.Add(new Item("Dagger", 0.8f));
+		fullItemList.Add(new Item("Emerald", 0.2f));
+		fullItemList.Add(new Item("Fish", 2.0f));
+		fullItemList.Add(new Item("Gems", 0.3f));
+		fullItemList.Add(new Item("Hat", 0.6f));
+		fullItemList.Add(new Item("Ingot", 5.0f));
+		fullItemList.Add(new Item("Junk", 1.2f));
+	}
 
-    }
-
-    void InventoryItemClicked(int index)
-    {
-        Debug.Log("Item Cicked: " + index + ". " + inventoryItemList[index].Name + " (" + inventoryItemList[index].Weight + ")");
-    }
-
-    void AddItemToInventory(int index)
+	void InitialiseFullItemList()
 	{
-        Item item = new Item(fullItemList[index].Name, fullItemList[index].Weight);
-        inventoryItemList.Add(item);
+		GameObject gameObject;
 
-        InitialiseInventoryItemList();
-    }
+		for (int i = 0; i < fullItemList.Count; i++)
+		{
+			gameObject = Instantiate(itemPrefab, transform);
+			gameObject.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = fullItemList[i].Name;
+			gameObject.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = fullItemList[i].Weight.ToString();
+			gameObject.GetComponent<Button>().AddEventListener(i, ItemClicked);
+		}
+	}
+
+	void InitialiseInventoryItemList()
+	{
+		ClearInventoryItemList();
+		GameObject gameObject;
+
+		for (int i = 0; i < inventoryItemList.Count; i++)
+		{
+			gameObject = Instantiate(itemPrefab, inventoryTransform);
+			gameObject.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = inventoryItemList[i].Name;
+			gameObject.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = inventoryItemList[i].Weight.ToString();
+			gameObject.GetComponent<Button>().AddEventListener(i, InventoryItemClicked);
+		}
+	}
+
+	void ClearInventoryItemList()
+	{
+		foreach (Transform child in inventoryTransform)
+		{
+			GameObject.Destroy(child.gameObject);
+		}
+	}
+
+	void ItemClicked(int index)
+	{
+		Debug.Log("Item Cicked: " + index + ". " + fullItemList[index].Name + " (" + fullItemList[index].Weight + ")");
+		AddItemToInventory(index);
+
+	}
+
+	void InventoryItemClicked(int index)
+	{
+		Debug.Log("Item Cicked: " + index + ". " + inventoryItemList[index].Name + " (" + inventoryItemList[index].Weight + ")");
+	}
+
+	void AddItemToInventory(int index)
+	{
+		Item item = new Item(fullItemList[index].Name, fullItemList[index].Weight);
+		inventoryItemList.Add(item);
+
+		InitialiseInventoryItemList();
+	}
 }
 
 public static class ButtonExtension
 {
-    public static void AddEventListener<T>(this Button button, T param, Action<T> OnClick)
+	public static void AddEventListener<T>(this Button button, T param, Action<T> OnClick)
 	{
-        button.onClick.AddListener(delegate() { OnClick(param); });
-    }
+		button.onClick.AddListener(delegate () { OnClick(param); });
+	}
 }
 
 public class Item
 {
-    public string Name { get; set; }
-    public float Weight { get; set; }
+	public string Name { get; set; }
+	public float Weight { get; set; }
 
-    public Item(string name, float weight)
-    {
-        Name = name;
-        Weight = weight;
-    }
+	public Item(string name, float weight)
+	{
+		Name = name;
+		Weight = weight;
+	}
 }
